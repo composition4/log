03fa87e58d6deb39ef4dc2356c8f153234700737
Josh Avanier <joshavanier@gmail.com>
2017-07-09 22:06:22 +0700

    Update log

--- log.js
+++ log.js
@@ -39,4 +39,5 @@ var log = [
 {"n":"65c02780","s":1840,"e":1854,"c":"COD","t":"MONOCAL","d":"Fixing script error"},
 {"n":"65c02791","s":1857,"e":1901,"c":"WRT","t":"Blog","d":"Updating MONOCAL post"},
 {"n":"65c02838","s":2024,"e":2049,"c":"COD","t":"GitHub","d":"Cleaning up code, updating readme's"},
+{"n":"65c028a9","s":2137,"e":2206,"c":"WEB","t":"Tumblr","d":"Updating my Tumblrs"},
 ]
