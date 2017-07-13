f1991e8408e562be778ababd612aa5aa39445853
Josh Avanier <joshavanier@gmail.com>
2017-07-13 11:06:41 +0700

    Update log

--- log.js
+++ log.js
@@ -72,4 +72,5 @@ var log = [
 {"n":"65c09c48","s":1736,"e":1932,"c":"WRT","t":"Worldbuild","d":"Worldbuilding"},
 {"n":"65c09d74","s":2036,"e":2124,"c":"COD","t":"Macian","d":"Working on Macian"},
 {"n":"65c09dcc","s":2124,"e":2143,"c":"PHO","t":"Photos","d":"Photo editing"},
+{"n":"65c0c022","s":0914,"e":1106,"c":"RES","t":"Mithvaer","d":"Worldbuilding"},
 ]
