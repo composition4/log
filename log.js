c460279fb1902e94193efef3bb5263316eae0344
Josh Avanier <joshavanier@gmail.com>
2017-07-14 15:40:07 +0700

    Update log

--- log.js
+++ log.js
@@ -78,4 +78,5 @@ var log = [
 {"n":"65c0e7a6","s":1030,"e":1203,"c":"WRT","t":"Mithvaer","d":"Worldbuilding"},
 {"n":"65c0e87c","s":1244,"e":1332,"c":"COD","t":"Compendium","d":"Building a knowledge base for worldbuilding"},
 {"n":"65c0e929","s":1417,"e":1440,"c":"COD","t":"Automation","d":"Updating shell scripts"},
+{"n":"65c0e94e","s":1454,"e":1540,"c":"COD","t":"MONOCAL","d":"Porting to bash"},
 ]
