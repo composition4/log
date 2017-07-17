5500537d0b83339ef6a055a996bde3625dae3c28
Josh Avanier <joshavanier@gmail.com>
2017-07-17 21:00:47 +0700

    Update log

--- log.js
+++ log.js
@@ -98,4 +98,5 @@ var log = [
 {"n":"65c15ce7","s":1047,"e":1228,"c":"COD","t":"Texte","d":"Adding experimental features"},
 {"n":"65c15d9c","s":1228,"e":1327,"c":"WRT","t":"Texte","d":"Writing demo game"},
 {"n":"65c15e5c","s":1420,"e":1832,"c":"MNT","t":"Room","d":"Cleaning"},
+{"n":"65c160b1","s":2017,"e":2100,"c":"PHO","t":"Photos","d":"Editing"},
 ]
