3a131fdfce873927002bcb991ec769607ace2b4e
Josh Avanier <joshavanier@gmail.com>
2017-07-20 18:21:35 +0700

    Update log

--- log.js
+++ log.js
@@ -115,4 +115,5 @@ var log = [
 {"n":"65c1d1f0","s":1008,"e":1144,"c":"DSG","t":"Branding","d":"Updating personal branding"},
 {"n":"65c1d2c2","s":1218,"e":1450,"c":"WEB","t":"Primaire","d":"Updating personal site"},
 {"n":"65c1d3b3","s":1459,"e":1634,"c":"RES","t":"Mithvaer","d":"Worldbuilding"},
+{"n":"65c1d466","s":1638,"e":1821,"c":"PHO","t":"Photos","d":"Editing"},
 ]
