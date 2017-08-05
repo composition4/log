339c9a14da7634a53b6730951ec85e147659d4e2
Josh Avanier <joshavanier@gmail.com>
2017-08-05 17:27:19 +0700

    Update log

--- log.js
+++ log.js
@@ -167,4 +167,5 @@ var log = [
 {"n":"65cea358","s":1048,"e":1117,"c":"RES","t":"Efficiency","d":"Writing efficient code"},
 {"n":"65cea535","s":1525,"e":1734,"c":"MNT","t":"GitHub","d":"Cleaning"},
 {"n":"65cecb6f","s":1311,"e":1437,"c":"MNT","t":"Purity","d":"Puritifaction"},
+{"n":"65cecc37","s":1511,"e":1727,"c":"WRT","t":"Mithvaer","d":"Worldbuilding"},
 ]
