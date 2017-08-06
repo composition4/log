067afdea87ffee36fc97aab38477d8b924ff1422
Josh Avanier <joshavanier@gmail.com>
2017-08-06 21:09:56 +0700

    Update log

--- log.js
+++ log.js
@@ -169,4 +169,5 @@ var log = [
 {"n":"65cecb6f","s":1311,"e":1437,"c":"MNT","t":"Purity","d":"Puritifaction"},
 {"n":"65cecc37","s":1511,"e":1727,"c":"WRT","t":"Mithvaer","d":"Worldbuilding"},
 {"n":"65cecde6","s":1942,"e":2231,"c":"ART","t":"Pixel Art","d":"Creating pixel art"},
+{"n":"65cef4fd","s":1949,"e":2109,"c":"DSG","t":"Design","d":"Designing something for Jul"},
 ]
