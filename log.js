7fbf4fd0885aa8fd9d6b60309b034c63dac58b73
Josh Avanier <joshavanier@gmail.com>
2017-08-15 20:57:51 +0700

    Update log

--- log.js
+++ log.js
@@ -204,4 +204,5 @@ var log = [
 {"n":"65d0516e","s":1150,"e":1237,"c":"COD","t":"Quoin","d":"Resurrecting this dead project"},
 {"n":"65d053cb","s":1755,"e":1820,"c":"COD","t":"Estelle","d":"Creating a statistics web app"},
 {"n":"65d054c6","s":2006,"e":2028,"c":"COD","t":"Roguelike","d":"Attempting a roguelike build in JS"},
+{"n":"65d054de","s":2030,"e":2057,"c":"COD","t":"College","d":"Creating an RPG game"},
 ]
