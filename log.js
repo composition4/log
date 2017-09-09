e78f61b8af80d33766e3fd721dc568981bcacd0d
Josh Avanier <joshavanier@gmail.com>
2017-09-09 13:35:16 +0700

    Log

--- log.js
+++ log.js
@@ -244,4 +244,5 @@ var log = [
 {"n":"65de84d0","s":1808,"e":1808,"c":"COD","t":"Log","d":"Updating repository"},
 {"n":"65dea8ee","s":1054,"e":1114,"c":"DSG","t":"Badges","d":"Creating dev status badges"},
 {"n":"65dea93c","s":1132,"e":1245,"c":"DSG","t":"Badges","d":"Updating badges"},
+{"n":"65dea9e8","s":1304,"e":1335,"c":"COD","t":"Badges","d":"Creating info pages"},
 ]
