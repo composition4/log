04a27a7323a8a5c41351054b1233df96a3d60ccc
Josh Avanier <joshavanier@gmail.com>
2017-09-17 14:45:08 +0700

    Update

--- bin/log.sh
+++ bin/log.sh
@@ -3,38 +3,48 @@
 F=$HOME/Dropbox/Code/log
 L=$F/log.js
 T=$F/log.txt
-
 TIME=$(date +"%H%M")
+N=$(date +"%y%m%d%H%M")
+D=$(printf '%x\n' $N)
+C=$F/temp/cache.txt
 
-s() {
-  N=$(date +"%y%m%d%H%M")
-  D=$(printf '%x\n' $N)
-
-  read -p "Title: " TTL
-  read -p "Description: " DSC
-
-  echo "$(date +"%y%m%d%H%M")  $TIME  undefined  $1  $TTL  $DSC" >> $T
-
-  sed -i '$d' $L
-  printf "{\"n\":\"$D\",\"s\":$TIME,\"e\":undefined,\"c\":\"$1\",\"t\":\"$TTL\",\"d\":\"$DSC\"},\n]" >> $L
-
-  echo "LOG START: $TIME"
+addTime() {
+  sed -i -e "s/undefined/$D/g" $1
 }
 
-e() {
-  sed -i -e "s/undefined/$TIME/g" $L
-  sed -i -e "s/undefined/$TIME/g" $T
-  echo "LOG END: $TIME"
+cache() {
+  echo "$1" >> $C
+}
+
+if [ $1 = "e" ]; then
+  addTime $L
+  addTime $T
+  echo "END: $TIME"
+  read -p "Have you accomplished what you set out to do? (y/n) " FIN
+  if [ $FIN = "y" ]; then
+    read -r TITLE < $C
+    VERB=$(sed -n '2p' $C)
+    WORD="$(echo $VERB | head -n1 | sed -e 's/\s.*$//')"
+    TENSE="$(past $WORD)"
+    NEW="$TENSE $(echo "$VERB" | cut -d " " -f2-)"
+    rotonde w "$TITLE: $NEW"
+    rm $C
+  fi
+  cd $F
   git add --all
   git commit -m "Log"
-}
-
-p() {
+elif [ $1 = "p" ]; then
+  echo "Push to master"
   cd $F
   git push -u origin master
-}
-
-if [ $1 = "e" ]; then e
-elif [ $1 = "p" ]; then p
-else s
+elif [ $1 = "h" ]; then
+  echo "Format: log {category} {title} {description}"
+else
+  DSC="$3"
+  echo "$D  undefined  $1  $2  $DSC" >> $T
+  sed -i '$d' $L
+  printf "{\"s\":\"$D\",\"e\":\"undefined\",\"c\":\"$1\",\"t\":\"$2\",\"d\":\"$DSC\"},\n]" >> $L
+  cache $2
+  cache $DSC
+  echo "START: $TIME"
 fi
