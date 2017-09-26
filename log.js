4c60503717374d7558df9c14fb92be0b5c43229d
Josh Avanier <joshavanier@gmail.com>
2017-09-26 21:43:58 +0700

    Log

--- log.js
+++ log.js
@@ -3,4 +3,5 @@ var log = [
 {"s":"59ca243e","e":"59ca564a","c":"COD","t":"Log","d":"Working on data visualisation"},
 {"s":"59ca5ba6","e":"59ca6073","c":"PHO","t":"Photography","d":"Editing a moon photo"},
 {"s":"59ca613b","e":"59ca6308","c":"PHO","t":"Photography","d":"Editing plant photos"},
+{"s":"59ca66b4","e":"59ca67ab","c":"COD","t":"Log","d":"Fixing vis for IE"},
 ]
