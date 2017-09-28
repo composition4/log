fb98bbb3aaeffe0c585a6248ffefcdb149c29df3
Josh Avanier <joshavanier@gmail.com>
2017-09-28 09:22:31 +0700

    Log

--- log.js
+++ log.js
@@ -16,4 +16,5 @@ var log = [
 {"s":"59cbc112","e":"59cbcba3","c":"DSG","t":"Heather","d":"Designing UI"},
 {"s":"59cbcdfe","e":"59cbd1cf","c":"COD","t":"Log","d":"Simplifying UI"},
 {"s":"59cbec67","e":"59cbee57","c":"COD","t":"Mors","d":"Adding progress bar"},
+{"s":"59cc5a0b","e":"59cc5ccd","c":"COD","t":"Log","d":"Adding log status indicator"},
 ]
