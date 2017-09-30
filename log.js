2d7a85561c507c8f95027ed86d458edd7f4ff8ed
Josh Avanier <joshavanier@gmail.com>
2017-09-30 15:50:21 +0700

    Log

--- log.js
+++ log.js
@@ -52,4 +52,5 @@ var log = [
 {"s":"59cf139b","e":"59cf2a55","c":"DSG","t":"Memex","d":"Updating site design"},
 {"s":"59cf2bd1","e":"59cf3a94","c":"COD","t":"Macian","d":"Adding utility classes"},
 {"s":"59cf422e","e":"59cf4597","c":"COD","t":"Macian","d":"Adding utility classes"},
+{"s":"59cf47ab","e":"59cf5acc","c":"COD","t":"Macian","d":"Expanding Codex contents"},
 ]
