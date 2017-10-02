15b03ba6851f2d7a6cca03bc927e1cbe397fe6a0
Josh Avanier <joshavanier@gmail.com>
2017-10-02 18:35:07 +0700

    Log

--- log.js
+++ log.js
@@ -85,4 +85,5 @@ var log = [
 {"s":"59d1f84d","e":"59d203d6","c":"COD","t":"Log","d":"Cleaning up code"},
 {"s":"59d2029d","e":"59d203d6","c":"ART","t":"Inktober","d":"Day 2 - Divided"},
 {"s":"59d20cad","e":"59d2135a","c":"ART","t":"Inktober","d":"Day 2 - Divided"},
+{"s":"59d215b1","e":"59d22469","c":"DSG","t":"Memex","d":"Updating design"},
 ]
