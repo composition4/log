6ec81e78a8c7191923a5b13ba0f1567185c4eda3
Josh Avanier <joshavanier@gmail.com>
2017-10-13 21:52:25 +0700

    Log

--- log.js
+++ log.js
@@ -184,4 +184,5 @@ var log = [
 {"s":"59e081a6","e":"59e08b93","c":"DSG","t":"Log","d":"Adding projects section"},
 {"s":"59e09255","e":"59e09829","c":"RES","t":"npm","d":"Learning how to use npm"},
 {"s":"59e0c083","e":"59e0ca40","c":"COD","t":"Log","d":"Adding web console"},
+{"s":"59e0cad6","e":"59e0d328","c":"COD","t":"Log","d":"Adding web console"},
 ]
