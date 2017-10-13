ac4edccc6ad9233dcc4834ba3b0fcf5107d5ccd7
Josh Avanier <joshavanier@gmail.com>
2017-10-13 14:24:57 +0700

    Log

--- log.js
+++ log.js
@@ -180,4 +180,5 @@ var log = [
 {"s":"59e0411d","e":"59e04607","c":"COD","t":"Somnus","d":"Optimising code"},
 {"s":"59e046b4","e":"59e050b0","c":"COD","t":"Log","d":"Adding sector stats"},
 {"s":"59e05b48","e":"59e061f6","c":"COD","t":"Rotonde","d":"Setting up Beaker"},
+{"s":"59e06408","e":"59e06a49","c":"COD","t":"Log","d":"Cleaning up code"},
 ]
