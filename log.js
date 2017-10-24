80d53097b291dcf39fe9ade6768d4ea685920999
Josh Avanier <joshavanier@gmail.com>
2017-10-24 09:52:33 +0700

    Log

--- log.js
+++ log.js
@@ -292,4 +292,5 @@ var log = [
 {s:"59edf977",e:"59edfc99",c:"DSG",t:"Log",d:"Updating month chart"},
 {s:"59ee0323",e:"59ee05c8",c:"COD",t:"Log",d:"Fixing month chart centering issue"},
 {s:"59ee955b",e:"59eea1e7",c:"COD",t:"Log",d:"Cleaning up code"},
+{s:"59eea3a3",e:"59eeaaf0",c:"COD",t:"Log",d:"Fixing chart overflow"},
 ]
