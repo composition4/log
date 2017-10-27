2e4df6473b992ca3c6795a1011b3223f7b512609
Josh Avanier <joshavanier@gmail.com>
2017-10-27 09:09:34 +0700

    Log

--- log.js
+++ log.js
@@ -324,4 +324,5 @@ var log = [
 {s:"59f1d680",e:"59f1dd18",c:"COD",t:"Log",d:"Building Electron app"},
 {s:"59f1dd51",e:"59f1e303",c:"COD",t:"Log",d:"Adding Log.console"},
 {s:"59f1e338",e:"59f1ec0e",c:"COD",t:"Log",d:"Adding custom settings"},
+{s:"59f285f5",e:"59f2955a",c:"COD",t:"Log",d:"Working on console"},
 ]
