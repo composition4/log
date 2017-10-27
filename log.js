684307d2dec90753a244f4b8ffbb34f4f81ec9f6
Josh Avanier <joshavanier@gmail.com>
2017-10-27 15:17:26 +0700

    Log

--- log.js
+++ log.js
@@ -328,4 +328,5 @@ var log = [
 {s:"59f2a73c",e:"59f2ad6f",c:"COD",t:"Log",d:"Implementing shelljs"},
 {s:"59f2b1c4",e:"59f2c2b6",c:"COD",t:"Log",d:"Implementing console commands"},
 {s:"59f2c543",e:"59f2d1cc",c:"COD",t:"Log",d:"Implementing console commands"},
+{s:"59f2da64",e:"59f2eb94",c:"COD",t:"Log",d:"Implementing console commands"},
 ]
