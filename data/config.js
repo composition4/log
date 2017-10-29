64932ae02b511bd37c9f08b3d837c80a68c8eca5
Josh Avanier <joshavanier@gmail.com>
2017-10-29 22:41:23 +0700

    Add data files

---
+++ data/config.js
@@ -0,0 +1,14 @@
+var config = {
+  ui: {
+		bg: "#202020",
+		colour: "#f8f8f8",
+    font: "sans-serif",
+		icons: false,
+		view: 28,
+  },
+  system: {
+		calendar: "gregorian",
+		timeFormat: "24",
+    weekStart: "Sunday"
+  }
+}
